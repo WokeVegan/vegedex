089e5e94327d8e5d5fd3b3d1340c277034e5d13f
Vegenaise <nrp97@gmail.com>
2023-08-14 03:45:28 -0400

    updated html

--- core/views.py
+++ core/views.py
@@ -4,3 +4,27 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+
+# Create your views here.
+def about(request):
+    return render(request, 'core/about.html')
+
+
+# Create your views here.
+def login(request):
+    return render(request, 'core/login.html')
+
+
+# Create your views here.
+def register(request):
+    return render(request, 'core/register.html')
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+
+def handler404(request, exception):
+    return render(request, 'core/404.html')
--- vegedex/urls.py
+++ vegedex/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, login, register, handler404
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
     path('admin/', admin.site.urls),
 ]
+
+handler404 = handler404
